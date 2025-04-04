e9c1710c638a1934e5f3a0fcf72e76f1be36bf53
gogo <hitmangogo7@gmail.com>
2025-04-04 12:12:58 +0530

    architect backend for the project

---
+++ backend/controllers/fileController.js
@@ -0,0 +1,41 @@
+const File = require('../models/File.js')
+const cloudinary = require('cloudinary')
+
+exports.uploadFile = async (req, res) => {
+    const { accessId } = req.body;
+
+    if(!req.file || !accessId){
+        return res.status(400).json({ message: 'File and accessId is required'})
+    }
+
+    try {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+            folder: 'file_share',
+        })
+
+        const file = new File({
+            accessId,
+            cloudinaryUrl: result.secure_url,
+            publicId: result.public_Id,
+        })
+
+        await file.save()
+        res.status(201).json({message: "File uploaded", accessId})
+    } catch (error) {
+        res.status(500).json({message: 'Upload failed', error: error.message})
+    }
+}
+
+exports.getFileByAccessId = async (req, res) => {
+    const { accessId } = req.params;
+
+    try{
+        const file = await File.findOne({ accessId });
+
+        if(!file) return res.status(404).json({ message: "file not found"})
+
+        res.status(200).json({ fileUrl: file.cloudinaryUrl});
+    }catch(error){
+        res.status(500).json({message: "Error retreving file", error: error.message})
+    }
+}
